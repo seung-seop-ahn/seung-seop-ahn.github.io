cf71cd56c29bec4e33fc68d4e317830ec9c13f7d
seung-seop-ahn <seungseopahn@gmail.com>
2023-12-03 19:34:38 +0900

    fix: next export config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
   output: 'export',
-  // images: {
-  //     loader: 'custom',
-  //     loaderFile: './my-loader.ts',
-  // },
+  // https://nextjs.org/docs/app/building-your-application/deploying/static-exports#configuration
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 }
 
 module.exports = nextConfig
